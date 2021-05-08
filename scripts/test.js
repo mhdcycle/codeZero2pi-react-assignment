8ce5e76512e972ecd9a6274e3c7d046d32f0bdb4
Mohammed Cyclewala <mohammedcyclewala.cm@gmail.com>
2021-05-08 07:24:42 +0530

    ..........

--- scripts/test.js
+++ scripts/test.js
@@ -13,8 +13,7 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
-
+// require('../config/env');
 
 const jest = require('jest');
 const execSync = require('child_process').execSync;
@@ -39,15 +38,10 @@ function isInMercurialRepository() {
 }
 
 // Watch unless on CI or explicitly running all tests
-if (
-  !process.env.CI &&
-  argv.indexOf('--watchAll') === -1 &&
-  argv.indexOf('--watchAll=false') === -1
-) {
+if (!process.env.CI && argv.indexOf('--watchAll') === -1 && argv.indexOf('--watchAll=false') === -1) {
   // https://github.com/facebook/create-react-app/issues/5210
   const hasSourceControl = isInGitRepository() || isInMercurialRepository();
   argv.push(hasSourceControl ? '--watch' : '--watchAll');
 }
-
 
 jest.run(argv);
